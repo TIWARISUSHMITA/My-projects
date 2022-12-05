1acfc1b97f31428cc9ef6d6bbef599362afcf03d
TIWARISUSHMITA <sushmitatiwari.rewa@gmail.com>
2022-12-05 16:35:38 +0530

    okii

---
+++ basices/arrays/Events/one.js
@@ -0,0 +1,3 @@
+function display() {
+    alert("Addition! not working");
+}
---
+++ basices/arrays/test11.js
@@ -0,0 +1,5 @@
+//to add the elemwnt
+let aar=[10,20,30,40,50];
+let arrSum=0;
+aar.forEach(elemtnt=>{arrSum=arrSum+elemtnt});
+console.log(arrSum);
