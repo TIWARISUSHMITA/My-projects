db14bd7c0dc6387b8d88d21d3f4a34cc299a97a9
TIWARISUSHMITA <sushmitatiwari.rewa@gmail.com>
2022-12-06 15:04:00 +0530

    okii

---
+++ basices/Events/event1/one.js
@@ -0,0 +1,4 @@
+console.log(document)
+console.log(document.getElementById('abc'));
+document.getElementById('abc').innerHTML=new Date().toLocaleDateString();
+//document.getElementById('abc').innerHTML="Hello,gm";
---
+++ basices/Events/event3/one.js
@@ -0,0 +1,5 @@
+function wish() {
+    let ref=document.getElementById('abc')
+    console.log(ref);
+    ref.style.backgroundColor="green";
+}
